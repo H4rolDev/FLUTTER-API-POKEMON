3f33118411fa04c609a24937f23550809789d085
LAB_A_REAL <laba@continental.edu.pe>
2025-01-30 18:04:39 -0500

    First commit

---
+++ lib/routes.dart
@@ -0,0 +1,18 @@
+import 'package:app1/models/user.dart';
+import 'package:app1/screens/home-page.dart';
+import 'package:app1/screens/login-page.dart';
+import 'package:app1/screens/minipokedex.dart';
+import 'package:app1/screens/profile-page.dart';
+import 'package:app1/screens/recovery-password.dart';
+import 'package:flutter/material.dart';
+
+Map<String, Widget Function(BuildContext)> get appRoutes {
+  return {
+    "home": (_) => HomePage(),
+    "login": (_) => LoginPage(),
+    "recovery-password": (_) => RecoveryPasswordPage(),
+    "profile": (context) => ProfilePage(
+        user: ModalRoute.of(context)!.settings.arguments as AppUser),
+    "pokedex": (_) => MiniPokedexPage(),
+  };
+}
